95722a7adbb393620c2e21d72bcd8a837764f6a7
Lorena Muralha <mlmuralha2301@gmail.com>
2024-02-21 11:39:25 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/kingCode/Project1/entities/OrderItem.java
+++ src/main/java/com/kingCode/Project1/entities/OrderItem.java
@@ -39,7 +39,16 @@ public class OrderItem {
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-
+	
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+	
 	public Integer getQuantity() {
 		return quantity;
 	}
@@ -55,6 +64,7 @@ public class OrderItem {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
 
 	@Override
 	public int hashCode() {
